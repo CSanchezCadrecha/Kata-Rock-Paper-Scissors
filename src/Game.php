ac5594fa30fb29666276cf3a65088119231618ed
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-08-23 09:34:45 +0200

    composer.lock

--- src/Game.php
+++ src/Game.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Game {
     
-    public function start($player1,$player2)
+    public function start($player1,$player2) : string
     {
         if($player1->getFigure()->getType() =="Rock" && 
         $player2->getfigure()->getType() == "Sicssors") 
